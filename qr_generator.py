436f4c75374076a03e1fb0ccdf15f198e1e76368
StefanoVianello <62439260+StefanoVianello@users.noreply.github.com>
2020-07-20 19:11:13 +0200

    Setup

---
+++ qr_generator.py
@@ -0,0 +1,34 @@
+import pyqrcode
+from PIL import Image
+import png
+
+# TODO make into a function with url and image strings + filename of finalprodut
+# TODO add way of iterating over barcodes
+
+# make QR code from custom string
+qrobj = pyqrcode.QRCode('https://fvvianello.github.io/pymol_ar/', error='H')
+with open('output/test2_qr.png', 'wb') as f:
+    qrobj.png(f, scale=10)
+
+# open the QR code to place the logo in the middle
+img = Image.open('output/test2_qr.png')
+width, height = img.size
+
+# open the logo image + white box image
+logo = Image.open('barcodes/6-barcode.png')
+square = Image.open('barcodes/white_square.png')
+
+# place square, then logo on top
+square_size = 150
+xmin_square = ymin_square = int((width / 2) - (square_size / 2))
+xmax_square = ymax_square = int((width / 2) + (square_size / 2))
+square = square.resize((xmax_square - xmin_square, ymax_square - ymin_square))
+img.paste(square, (xmin_square, ymin_square, xmax_square, ymax_square))
+
+logo_size = 100
+xmin = ymin = int((width / 2) - (logo_size / 2))
+xmax = ymax = int((width / 2) + (logo_size / 2))
+logo = logo.resize((xmax - xmin, ymax - ymin))
+img.paste(logo, (xmin, ymin, xmax, ymax))
+
+img.save('output/test3_qr.png')
